7f6f4c4bd01896d85612f9515443f9337c567a6e
jonlu <jonlu@yeahka.com>
2024-05-18 19:23:18 +0800

    优化系统性灰度Hint相关功能

--- spring-cloud-wendaoren/spring-cloud-wendaoren-loadbalancer/src/main/java/com/wendaoren/springcloud/loadbalancer/support/hint/HintRequestFilter.java
+++ spring-cloud-wendaoren/spring-cloud-wendaoren-loadbalancer/src/main/java/com/wendaoren/springcloud/loadbalancer/support/hint/HintRequestFilter.java
@@ -1,5 +1,6 @@
 package com.wendaoren.springcloud.loadbalancer.support.hint;
 
+import com.wendaoren.springcloud.loadbalancer.constant.LoadBalancerConstant;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class HintRequestFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
-            HintContext.set(httpRequest.getHeader(HintContext.HINT_ATTR_NAME));
+            HintContext.set(httpRequest.getHeader(LoadBalancerConstant.REQUEST_CONTEXT_HINT_NAME));
         }
         filterChain.doFilter(request, response);
         HintContext.remove();
